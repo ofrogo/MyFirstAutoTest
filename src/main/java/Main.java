d17a6126f5bda09a2cfd0653af064bfb4be37f93
Daniil Shatalov <DaniilShat@yandex.ru>
2019-09-17 20:03:24 +0400

    add configs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,32 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+
+        Properties properties = new Properties();
+        try (InputStream inputStream = new FileInputStream("/home/danil/IdeaProjects/MyFirstTest/src/main/resources/config.properties")){
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         WebDriver webDriver = new ChromeDriver();
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         webDriver.get("https://beru.ru");
-        webDriver.findElement(new By.ByXPath("/html/body/div[1]/div[2]/div[1]/div/div/div/div/div/div" +
-                "/div/div/span/div/div[3]/div[1]/div/div[3]/div/div/div")).click();
+        webDriver.findElement(new By.ByCssSelector("body > div.main > div:nth-child(2) > div:nth-child(1) > div > div > div > div > div > div > div > div > span > div > div._1SEkJje5GJ > div._2BUQxcqKF7 > div > div.Mvy4Zvr556 > div > div > div > a")).click();
         WebElement webElement = webDriver.findElement(By.id("passp-field-login"));
-        webElement.sendKeys("DaniilShat@yandex.ru"); //TODO login to config file
+        webElement.sendKeys(properties.getProperty("yandex.email"));
         webDriver.findElement(new By.ByCssSelector("#root > div > div > div.passp-flex-wrapper > div > div.passp-auth > div.passp-auth-content > div:nth-child(2) > div > div > div.passp-login-form > form > div.passp-button.passp-sign-in-button > button.control.button2.button2_view_classic.button2_size_l.button2_theme_action.button2_width_max.button2_type_submit.passp-form-button")).click();
         webElement = webDriver.findElement(By.id("passp-field-passwd"));
-        webElement.sendKeys("111111111111"); //TODO password to config file
+        webElement.sendKeys(properties.getProperty("yandex.password"));
         webElement.submit();
 
     }
