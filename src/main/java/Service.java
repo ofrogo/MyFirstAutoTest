eb7085c97c868d6655b4789dc52431ec1c81e495
Daniil Shatalov <DaniilShat@yandex.ru>
2019-09-18 11:29:07 +0400

    Create second test

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,13 +1,7 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-
-import static com.sun.activation.registries.LogSupport.log;
 
 public class Service {
 
@@ -20,15 +14,5 @@ public class Service {
             e.printStackTrace();
         }
         return properties;
-    }
-
-    public static void safeClick(WebDriver webDriver, String elementLocator) {
-        WebElement webElement = webDriver.findElement(By.cssSelector(elementLocator));
-        if (webElement != null) {
-            webElement.click();
-        } else {
-            System.out.println("Element: " + elementLocator + ", is not available on page - "
-                    + webDriver.getCurrentUrl());
-        }
     }
 }
--- src/main/java/TestBeru.java
+++ src/main/java/TestBeru.java
@@ -1,13 +1,18 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public class TestBeru {
@@ -26,15 +31,15 @@ public class TestBeru {
 
     @Test
     public void firstAuthorization() {
-        Service.safeClick(webDriver, "body > div.main > div:nth-child(2) > div:nth-child(1) > div > " +
+        webDriver.findElement(By.cssSelector("body > div.main > div:nth-child(2) > div:nth-child(1) > div > " +
                 "div > div > div > div > div > div > div > span > div > div._1SEkJje5GJ > div._2BUQxcqKF7 > div > " +
-                "div.Mvy4Zvr556 > div > div > div > a");
+                "div.Mvy4Zvr556 > div > div > div > a")).click();
         WebElement webElement = webDriver.findElement(By.id("passp-field-login"));
         webElement.sendKeys(account.getEmail());
-        Service.safeClick(webDriver, "#root > div > div > div.passp-flex-wrapper > div > div.passp-auth > " +
+        webDriver.findElement(By.cssSelector("#root > div > div > div.passp-flex-wrapper > div > div.passp-auth > " +
                 "div.passp-auth-content > div:nth-child(2) > div > div > div.passp-login-form > form > " +
                 "div.passp-button.passp-sign-in-button > button.control.button2.button2_view_classic.button2_size_l." +
-                "button2_theme_action.button2_width_max.button2_type_submit.passp-form-button");
+                "button2_theme_action.button2_width_max.button2_type_submit.passp-form-button")).click();
         webElement = webDriver.findElement(By.id("passp-field-passwd"));
         webElement.sendKeys(account.getPassword());
         webElement.submit();
@@ -45,33 +50,48 @@ public class TestBeru {
                         return d.findElement(By.xpath("//span[@class='pFhTbV17qj']"));
                     }
                 });
-        String res = myDynamicElement.getAttribute("textContent");
-        if (res.equals("Мой профиль")) {
-            System.out.println("Authorization is ok.");
-        } else {
-            System.out.println("Authorization is not ok.");
-        }
+        Assert.assertEquals("Мой профиль", myDynamicElement.getAttribute("textContent"));
     }
 
     @Test
-    public void secondChangeCity() {
-        Service.safeClick(webDriver, "body > div.main > div:nth-child(2) > div:nth-child(1) > div > div > " +
+    public void secondChangeCity() throws InterruptedException {
+        webDriver.findElement(By.cssSelector("body > div.main > div:nth-child(2) > div:nth-child(1) > div > div > " +
                 "div > div > div > div > div > div > span > div > div._1SEkJje5GJ > div._2cfUiVAU6V > div > div > " +
-                "div > div > span > span._2XJ6yiRp5w");
+                "div > div > span > span._2XJ6yiRp5w")).click();
         WebElement webElement = (new WebDriverWait(webDriver, 10))
                 .until(new ExpectedCondition<WebElement>() {
                     @Override
                     public WebElement apply(WebDriver d) {
-                        return d.findElement(By.xpath("//*[@id=\"textfield8070504168\"]"));
+                        return d.findElement(By.cssSelector("input[data-tid=\"37e0ab2d\"]"));
                     }
                 });
-        webElement.clear();
+        webElement.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
         webElement.sendKeys("Хвалынск");
-        webElement.submit();
+        webElement = (new WebDriverWait(webDriver, 10))
+                .until(new ExpectedCondition<WebElement>() {
+                    @Override
+                    public WebElement apply(WebDriver d) {
+                        return d.findElement(By.cssSelector("#react-autowhatever-region--item-0"));
+                    }
+                });
+        webElement.click();
+        webElement = (new WebDriverWait(webDriver, 10))
+                .until(new ExpectedCondition<WebElement>() {
+                    @Override
+                    public WebElement apply(WebDriver d) {
+                        return d.findElement(By.cssSelector("._4qhIn2-ESi.Pjv3h3YbYr.THqSbzx07u"));
+                    }
+                });
+        webElement.click();
+        Thread.sleep(5000); // TODO change
+        String city = webDriver.findElement(By.cssSelector("body > div.main > div:nth-child(2) > div:nth-child(1) > " +
+                "div > div > div > div > div > div > div > div > span > div > div._1SEkJje5GJ > div._2cfUiVAU6V > " +
+                "div > div > div > div > span > span._2XJ6yiRp5w")).getAttribute("textContent");
+        Assert.assertEquals("Хвалынск", city);
     }
 
     @After
     public void tearDown() {
-        webDriver.quit();
+        //webDriver.quit();
     }
 }
